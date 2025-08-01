01ad775c6d0b5939dbf063ea7867a5b63fb0ec6b
Matojeje <mato@riolu.tf>
2025-08-02 00:30:53 +0200

    Add sounds to world map

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -52,6 +52,7 @@ const images: Image[] = [
 	image("ui/cursor", "cursor"),
 	image("ui/lose", "lose"),
 	image("ui/win", "win"),
+	image("ui/question", "question"),
 
 	// World hub
 	image("hub/level", "hub_level"),
@@ -81,6 +82,10 @@ const audios: Audio[] = [
 	sound("drawing/tap", "d_tap"),
 	sound("drawing/break", "d_break"),
 	sound("tree/machinegun", "machinegun", 0.5),
+
+	sound("ui/screen_transition", "u_level_enter"),
+	sound("ui/what", "u_question"),
+	sound("ui/disabled", "u_disabled"),
 ];
 
 /* Fonts */
