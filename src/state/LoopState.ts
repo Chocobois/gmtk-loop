3b12f0c4f9a3872e8df48f89553733954cce4b41
Golen <golen87@hotmail.com>
2025-08-03 16:07:55 +0200

    Make Fire pearl boost attack by x1.5

--- src/state/LoopState.ts
+++ src/state/LoopState.ts
@@ -1,14 +1,24 @@
 import { makeAutoObservable } from "mobx";
+import { pearlState } from "./PearlState";
+import { PearlElement } from "@/components/pearls/PearlElement";
 
 class LoopState {
 	health = 100; // Current player health
 	maxHealth = 100; // Maximum player health
 
 	maxLength = 1600; // Maximum length of the loop
-	attackPower = 100; // Player loop damage
 
 	constructor() {
 		makeAutoObservable(this);
+	}
+
+	// Player loop damage
+	get attackPower() {
+		if (pearlState.currentPearl.element == PearlElement.Fire) {
+			return 150;
+		} else {
+			return 100;
+		}
 	}
 }
 
